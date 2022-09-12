1642a175c9997c9be83b67d480b67df826a43238
Jorge Gonzaga <jsgonzaga@uol.com.br>
2022-09-12 18:01:46 -0300

    desafio concluido

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -4,51 +4,60 @@ import { FiCheckSquare } from 'react-icons/fi';
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FormHandles } from '@unform/core/typings/types';
 
-interface EditFood {
+interface AddFood {
   image: string;
   name: string;
   price: string;
   description: string;
 }
 
+interface IFood{
+  id:number;
+  name:string;
+  description:string;
+  price:number;
+  available:boolean;
+  image:string;
+}
+
 interface ModalEditFoodProps{
   isOpen: boolean;
   setIsOpen: () => void;
-  handleUpdateFood: (data: EditFood) => void;
+  handleUpdateFood: (data: AddFood) => void;
+  editingFood: IFood;
 }
 
-export default function ModalEditFood({isOpen, setIsOpen, handleUpdateFood}: ModalEditFoodProps) {
+export default function ModalEditFood({isOpen, setIsOpen, handleUpdateFood, editingFood}: ModalEditFoodProps) {
+  const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = async(data: EditFood) => {
+  const handleSubmit = async(data: AddFood) => {
     handleUpdateFood(data);
     setIsOpen();
-  }
+  };
 
-  const formRef = useRef(null);
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
 
-  const editingFood = {};
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
 
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
-          <h1>Editar Prato</h1>
-          <Input name="image" placeholder="Cole o link aqui" />
+        <Input name="description" placeholder="Descrição" />
 
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
 
-          <Input name="description" placeholder="Descrição" />
-
-          <button type="submit" data-testid="edit-food-button">
-            <div className="text">Editar Prato</div>
-            <div className="icon">
-              <FiCheckSquare size={24} />
-            </div>
-          </button>
-        </Form>
-      </Modal>
-    );
   }
 
 
